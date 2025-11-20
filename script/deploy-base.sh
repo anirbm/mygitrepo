dae4b39315ad69c1aca3d4a2f42fdb518e2c595d
anirbm <anirban.m12@gmail.com>
2025-11-20 13:29:29 +1100

    Remove DOCKER_IMAGE check from deploy script
    Removed check for missing DOCKER_IMAGE variable.

--- script/deploy-base.sh
+++ script/deploy-base.sh
@@ -2,15 +2,6 @@
 
 set -euo pipefail
 
-if [ -z "${DOCKER_IMAGE:-}" ]; then
-  echo ":boom: \$DOCKER_IMAGE missing" 1>&2
-  exit 1
-fi
-
-manifest="$(mktemp)"
-
 echo '--- :kubernetes: Current Context'
-
-envsubst < k8s/deployment.yml > "${manifest}"
 kubectl config current-context
 echo '--- :zzz: End of jobs'
